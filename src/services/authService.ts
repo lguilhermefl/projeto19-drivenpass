55c756e408d5a5971071691a5244c907f417a200
luiz <lguilhermefl@gmail.com>
2022-09-12 07:41:20 -0300

    feat: add auth service

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,11 +1,12 @@
 import * as userRepository from "../repositories/userRepository";
 import { UserInsertData } from "../types/userTypes";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export async function signUp(user: UserInsertData) {
   const { email, password }: UserInsertData = user;
 
-  checkSignUpEmail(email);
+  await checkSignUpEmail(email);
 
   const passwordHash: string = generateEncryptedPassword(password);
 
@@ -17,23 +18,58 @@ export async function signUp(user: UserInsertData) {
   await userRepository.insert(userData);
 }
 
-export async function signIn() {}
+export async function signIn(user: UserInsertData) {
+  const { email, password }: UserInsertData = user;
+  const userData: any = await getUserByEmail(email);
+
+  checkSignInEmail(userData);
+  checkSignInPassword(password, userData.password);
+
+  const token: string = generateJwtToken(email);
+
+  return token;
+}
 
 async function checkSignUpEmail(email: string) {
   const user: any = await getUserByEmail(email);
   if (user) throw { type: "Conflict", message: "This email is already in use" };
 }
 
-async function getUserByEmail(email: string) {
+async function getUserByEmail(email: string): Promise<any> {
   return await userRepository.findByEmail(email);
 }
 
-function generateEncryptedPassword(password: string) {
+function generateEncryptedPassword(password: string): string {
   const SALT = 10;
   return bcrypt.hashSync(password, SALT);
 }
 
-export function checkSignInPassword(password: string, cardPassword: string) {
-  if (!bcrypt.compareSync(password, cardPassword))
+async function checkSignInEmail(email: string) {
+  const user: any = await getUserByEmail(email);
+  if (!user)
+    throw { type: "Conflict", message: "This email is already in use" };
+}
+
+function checkSignInPassword(password: string, passwordHash: string) {
+  if (!bcrypt.compareSync(password, passwordHash))
     throw { code: "Unauthorized", message: "Incorrect password" };
+}
+
+function generateJwtToken(email: string): string {
+  const SECRET: string = process.env.TOKEN_SECRET_KEY ?? "";
+  const EXPIRES_IN = process.env.TOKEN_EXPIRES_IN;
+
+  const payload = {
+    id: 1,
+    email,
+    nivel: 1,
+  };
+
+  const jwtConfig = {
+    expiresIn: EXPIRES_IN,
+  };
+
+  const token = jwt.sign(payload, SECRET, jwtConfig);
+
+  return token;
 }
--- src/types/userTypes.ts
+++ src/types/userTypes.ts
@@ -1,3 +1,4 @@
 import { Users } from "@prisma/client";
 
+export type User = Users;
 export type UserInsertData = Omit<Users, "id">;
